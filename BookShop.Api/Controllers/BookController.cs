ddd5456ababde61181c8848e5a3525be78386bc8
Mostafa <mostafasalba2020@gmail.com>
2025-01-09 16:00:46 +0200

    Add Handle End Response With Custom Response <T>

--- BookShop.Api/Controllers/BookController.cs
+++ BookShop.Api/Controllers/BookController.cs
@@ -1,43 +1,34 @@
-﻿using BookShop.Core.Features.Books.Commands.Models;
+﻿using BookShop.Api.Base;
+using BookShop.Core.Features.Books.Commands.Models;
 using BookShop.Core.Features.Books.Queries.Models;
 using BookShop.DataAccess.AppMetaData;
-using MediatR;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookShop.Api.Controllers
 {
     //[Route("api/[controller]")] //I Made Custom Routing
     [ApiController]
-    public class BookController : ControllerBase
+    public class BookController : AppControllerBase
     {
-        private readonly IMediator _mediator;
-
-        public BookController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
-
         [HttpGet(Router.BookRouting.List)]
         public async Task<IActionResult> GetBooksList()
         {
-            var response = await _mediator.Send(new GetBookListQuery());
+            var response = await Mediator.Send(new GetBookListQuery());
             return Ok(response);
         }
 
         [HttpGet(Router.BookRouting.GetById)]
         public async Task<IActionResult> GetBookById([FromRoute] int id)
         {
-            var response = await _mediator.Send(new GetBookByIdQuery(id));
-            return Ok(response);
+            var response = await Mediator.Send(new GetBookByIdQuery(id));
+            return NewResult(response);
         }
 
         [HttpPost(Router.BookRouting.Create)]
         public async Task<IActionResult> Create([FromBody] AddBookCommand command)
         {
-            var response = await _mediator.Send(command);
-            return Ok(response);
+            var response = await Mediator.Send(command);
+            return NewResult(response);
         }
     }
 }
