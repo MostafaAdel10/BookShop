5bfed20d8ef3826b9c854f2cc5b36a81776489c2
Mostafa Adel <mostafa.adel.net@gmail.com>
2025-02-25 06:57:01 +0200

    Identity (#4)
    * Add Create User Endpoint in Identity, IHttpContextAccessor,IEmailsService,IUrlHelper And Add Implementation for them And Add UserRefreshToken Entity and Add column code in ApplicationUser Entity
    
    * Add Get and GetById Of IdentityUser
    
    * Add Edit User Endpoint
    
    * Add Delete User Endpoint
    
    * Add ChangePassword Endpoint
    
    ---------
    
    Co-authored-by: Mostafa <mostafasalba2020@gmail.com>

---
+++ BookShop.Core/Features/User/Commands/Models/ChangeUserPasswordCommand.cs
@@ -0,0 +1,13 @@
+﻿using BookShop.Core.Bases;
+using MediatR;
+
+namespace BookShop.Core.Features.User.Commands.Models
+{
+    public class ChangeUserPasswordCommand : IRequest<Response<string>>
+    {
+        public int Id { get; set; }
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
